80263e17053d49c545275f6368e9f60ad3b4638e
Tom Collin <collintom3@gmail.com>
2024-04-26 09:52:39 +0200

    fix refacto exercice 4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -5,15 +5,25 @@ import java.util.Date;
 
 public class DateUtils {
 
+    /**
+     * Formate une date selon le motif spécifié.
+     *
+     * @param pattern Le motif de formatage de la date, par exemple "dd/MM/yyyy HH:mm:ss".
+     * @param date La date à formater.
+     * @return La date formatée selon le motif spécifié.
+     */
     public static String format(String pattern, Date date) {
-
         SimpleDateFormat formateur = new SimpleDateFormat(pattern);
         return formateur.format(date);
     }
 
+    /**
+     * Formate une date selon un format par défaut.
+     *
+     * @param date La date à formater.
+     * @return La date formatée selon le format "dd/MM/yyyy HH:mm:ss".
+     */
     public static String formatDefaut(Date date) {
-
-        SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        return formateur.format(date);
+        return format("dd/MM/yyyy HH:mm:ss", date);
     }
 }
