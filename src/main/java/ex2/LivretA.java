a9b388b64b1fc4e492387ad28744729a4e638515
Tom Collin <collintom3@gmail.com>
2024-04-26 09:43:42 +0200

    fix refacto exercice 2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,31 @@
 package ex2;
 
+/**
+ * Classe représentant un Livret A, spécialisation de CompteBancaire.
+ */
 public class LivretA extends CompteBancaire {
+    private double tauxRemuneration;
 
-    public LivretA(String type, double solde, double tauxRemuneration) {
-        super(type, solde, 0, tauxRemuneration);
+    public LivretA(double solde, double tauxRemuneration) {
+        super(solde, 0);
+        this.tauxRemuneration = tauxRemuneration;
     }
 
+    @Override
+    public void debiterMontant(double montant) {
+        if (getSolde() >= montant) {
+            super.ajouterMontant(-montant);
+        } else {
+            System.out.println("Fonds insuffisants, impossible de retirer.");
+        }
+    }
+
+    public void appliquerRemuAnnuelle() {
+        double remuneration = getSolde() * tauxRemuneration;
+        super.ajouterMontant(remuneration);
+    }
+
+    public double getTauxRemuneration() {
+        return tauxRemuneration;
+    }
 }
