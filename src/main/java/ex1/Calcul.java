3f66b72d9079a889bf06a5ea89816efffe565b6a
Tom Collin <collintom3@gmail.com>
2024-04-26 09:31:35 +0200

    fix refacto

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,29 @@
+package ex1;
+
+/**
+ * Classe pour effectuer des opérations arithmétiques de base comme l'addition et la soustraction.
+ */
+public class Calcul {
+
+    /**
+     * Calcule la somme de deux nombres entiers.
+     *
+     * @param a le premier entier à additionner.
+     * @param b le deuxième entier à additionner.
+     * @return la somme de a et b.
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Calcule la différence entre deux nombres entiers.
+     *
+     * @param a le minuend.
+     * @param b le subtrahend.
+     * @return la différence entre a et b.
+     */
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
